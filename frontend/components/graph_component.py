6dc8fb237871c6599bfe0b209b0b5528d397a79a
Mahmoud Kamaleldin <amahmoud98@gmail.com>
2021-09-13 12:46:36 -0700

    Add style as argument for graph component

--- frontend/components/graph_component.py
+++ frontend/components/graph_component.py
@@ -4,7 +4,7 @@ import dash_html_components as html
 import plotly.express as px
 import pandas as pd
 
-def GraphComponent(graph_name, graph_id, interval_id, data={'x':[], 'y':[]}, graph_type="scatter", update_interval=1000):
+def GraphComponent(graph_name, graph_id, interval_id, style={} data={'x':[], 'y':[]}, graph_type="scatter", update_interval=1000):
   if graph_type == "scatter":
     fig = px.scatter(data['x'], data['y'],
                     labels={
@@ -20,5 +20,7 @@ def GraphComponent(graph_name, graph_id, interval_id, data={'x':[], 'y':[]}, gra
         id=graph_id, 
         animate=False, 
       ),
-    dcc.Interval(id=interval_id, interval=update_interval)
-  ])
+      dcc.Interval(id=interval_id, interval=update_interval)
+    ],
+    style=style
+  )
