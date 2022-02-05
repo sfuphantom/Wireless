fdd10e0dacd87b37d9c3fca1b5ac2334f524b675
cushingnathan37@gmail.com <73880893+nathan-37@users.noreply.github.com>
2022-02-04 18:56:28 -0800

    added intervals and multiple sensor support

--- dataSimulation/CSVreader.py
+++ dataSimulation/CSVreader.py
@@ -1,17 +1,30 @@
+from importlib.resources import path
 import pathlib
 import csv
 import json
 import pandas as pd
+import time
+import os
 
-#period in seconds
-dataPeriod = 1.0
-fileName = "testIMUdata.csv"
+endl = os.linesep
+
+#interval of which to send the corrosponding data in # of data points
+dataInterval = .05
+
+#frequency of given data in Hz(for testing purposes; this will be dynamic later on)
+dataFrequency = 63
+#fileName = "testIMUdata.csv"
+
+#all csvs to have test data created should be in this folder.
+simulationPath = "Wireless\\dataSimulation\\Data\\codeTesting"
+
 
 #will take in a dataPoint, and convert to a json
-dataFilepath = str(pathlib.Path().resolve()) + "\\Wireless\\dataSimulation\\Data\\" + fileName
-dataFile = open(dataFilepath, newline= '')
-sensorData = pd.read_csv(dataFile)
-
+def data_get(fileLocation):
+    #dataFilepath = str(pathlib.Path().resolve()) + "\\Wireless\\dataSimulation\\Data\\" + fileName
+    dataFile = open(fileLocation, newline= '')
+    sensorData = pd.read_csv(dataFile)
+    return sensorData
 """
 print(type(sensorData))
 print(sensorData)
@@ -20,47 +33,74 @@ print("\n")
 print(sensorData.iloc[0][0])
 print(sensorData.columns[0])
 """
+def data_print(sensorData, t_0, t_1):
+    #initial data point time
+    t0 = sensorData.iloc[0][0]
 
-#initial data point time
-t0 = sensorData.iloc[0][0]
+    #print (sensorData.iterrows())
+    #+1 is to offset that data begins on the second row
+    for i in range(t_0+1,t_1+1,1):
+        dataPointDICT = {}
+        for j in range(0, len(sensorData.columns), 1):
+            dataPointDICT[sensorData.columns[j]] = sensorData.iloc[i][j]
+    # dataPointJSON(this will need to be sent over mqtt later) = 
+        print(json.dumps(dataPointDICT))
 
-#print (sensorData.iterrows())
-for i in sensorData.index:
-    dataPointDICT = {
-        sensorData.columns[0]: sensorData.iloc[i][0],
-        sensorData.columns[1]: sensorData.iloc[i][1],
-        sensorData.columns[2]: sensorData.iloc[i][2],
-        sensorData.columns[3]: sensorData.iloc[i][3],
-        sensorData.columns[4]: sensorData.iloc[i][4],
-        sensorData.columns[5]: sensorData.iloc[i][5],
-        sensorData.columns[6]: sensorData.iloc[i][6],
-        sensorData.columns[7]: sensorData.iloc[i][7]
-    }
-    dataPointJSON = json.dumps(dataPointDICT)
-    print(dataPointJSON)
+
+#data_print()
+
+#print(os.listdir(str(pathlib.Path().resolve()) + "/Wireless/dataSimulation/Data"))
+#print os.listdir(path)
+ 
+#print(os.listdir(str(pathlib.Path().resolve()) + "\Wireless\dataSimulation\Data"))
+
+#print(os.listdir(simulationPath))
+    #dataPath = os.path.join(simulationPath, FileName)
+
+#(initial data point, final data point, interval in between)
+dataPointSetCount = 2
+for i in range (0,9,dataPointSetCount):
+    for dataFileName in os.listdir(simulationPath):
+        #print(os.path.join(simulationPath, dataFileName))
+
+        #initial and final points in terms of data points
+        t_0 = i
+        t_1 = i+dataPointSetCount
+        if dataFileName.endswith("Full.csv"):
+            sensorData = data_get(os.path.join(simulationPath, dataFileName))
+            print(endl + "Current data file: " + dataFileName)
+            data_print(sensorData, t_0, t_1)
+            #print(type(sensorData))
+    time.sleep(1.5)
+
+    
+#for j in range(0,4,1):
+    #print(j)
+
+#str(pathlib.Path().resolve()) + simulationPath + "\\codeTesting" 
+
+
+
 
 """
+    print(dataPointDICT)
+    print(type(dataPointDICT))
 
-print(dataPointDICT)
-print(type(dataPointDICT))
+    print(sensorData.index)
 
-print(sensorData.index)
+    dataPointJSON = json.dumps(dataPoint)
 
-dataPointJSON = json.dumps(dataPoint)
+    print(dataPointJSON)
+    print(type(dataPointJSON))
 
-print(dataPointJSON)
-print(type(dataPointJSON))
+    df = pd.read_csv(dataFile)
 
-df = pd.read_csv(dataFile)
+    print(df.to_string())
 
-print(df.to_string())
+    #initial time the data begins
+    #t0
 
-#initial time the data begins
-#t0
-
-dataPoint = {
-    
-}
-
-
+    dataPoint = {
+        
+    }
 """
